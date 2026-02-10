2b76f060ebf221fb2fc204f152bd5db37012c1e5
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-10 20:34:38 -0300

    interface practice

--- src/bases/03-object-literal.ts
+++ src/bases/03-object-literal.ts
@@ -1,18 +1,41 @@
-const ironman = {
+interface Person {
+    firstName: string
+    lastName: string
+    age: number
+    address: Address
+}
+
+interface Address {
+    postalCode: string
+    city: string
+}
+
+const ironman: Person = {
     firstName: 'Tony',
     lastName: 'Stark',
     age: 45,
     address: {
         postalCode: 'ABC123',
         city: 'New York',
-    }
+    },
 }
 
-const spiderman = structuredClone(ironman)
+// const spiderman: Person = {      Ctrl + .  
+//                                  Add missing properties
+// }
+// const spiderman: Person = {      Agrega las propiedades que ya
+//     firstName: "",               conoce de esa interfaz
+//     lastName: "",
+//     age: 0
+// }
 
-spiderman.lastName = 'Parker'
-spiderman.firstName = 'Peter'
-spiderman.age = 22
-spiderman.address.city = 'San José'
+console.log(ironman)
 
-console.log(ironman, spiderman)
+// const spiderman = structuredClone(ironman)
+
+// spiderman.lastName = 'Parker'
+// spiderman.firstName = 'Peter'
+// spiderman.age = 22
+// spiderman.address.city = 'San José'
+
+// console.log(ironman, spiderman)
