634acaad6207cb9db4c28eb7ca9b0e801446d7db
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-10 18:25:55 -0300

    create project reforzamiento

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName = 'Adrian'
+const lastName = 'Fayos'
+
+let diceNumber = 5
+diceNumber = 3
+
+const containsLetterF = lastName.includes('f')
+
+console.log({containsLetterF, diceNumber, firstName, lastName})
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+import './bases/01-const-let'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Hola Mundo</h1>
+  </div>
+`
